ee8447cb74f85c2cb2cf93f005f7686ee28d1122
AlexLundinEducational <31811167+AlexLundinEducational@users.noreply.github.com>
2018-04-24 00:06:17 -0500

    phase 4 97%

--- CDKmatrix.cc
+++ CDKmatrix.cc
@@ -46,17 +46,15 @@ int main()
 {
 
 	// binary IO
-	
 	std::string binaryRecordFile = "./cs3377.bin";
 
 	// read the header
-	
 	BinaryFileHeader *myHeader = new BinaryFileHeader();
-
 	ifstream binInfile (binaryRecordFile.c_str(), ios::in | ios::binary);
-
 	binInfile.read((char *) myHeader, sizeof(BinaryFileHeader));
 	const long HEADER_SIZE =  sizeof(BinaryFileHeader);
+	
+	// move the position for the file with seekg
 	binInfile.seekg (1 * HEADER_SIZE);
 	
 	
@@ -64,11 +62,9 @@ int main()
 	
 
 	// CDK Screen Matrix
-	
 	WINDOW	*window;
 	CDKSCREEN	*cdkscreen;
 	CDKMATRIX     *myMatrix;           
-
 	const char 		*rowTitles[MATRIX_HEIGHT+1] = {"R0", "R1", "R2", "R3", "R4", "R5"};
 	const char 		*columnTitles[MATRIX_WIDTH+1] = {"C0", "C1", "C2", "C3"};
 	int		boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
@@ -78,7 +74,6 @@ int main()
 
 	// Initialize the Cdk screen.
 	// Make sure the putty terminal is large enough
-
 	window = initscr();
 	cdkscreen = initCDKScreen(window);
 
@@ -87,7 +82,6 @@ int main()
 
 
 	// Create the matrix.
-
 	myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH,
 	MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
 	boxTypes, 1, 1, ' ', ROW, true, true, false);
@@ -107,35 +101,42 @@ int main()
 	myStream << std::hex << (uint32_t)myHeader->magicNumber;
 	std::string result( myStream.str() );
 
-	
 	// display magicNumber in matrix
 	setCDKMatrixCell(myMatrix, 1, 1, result.c_str());
 	
-	// convert the versionNumber to string, which is a integer represenation of a hex number
-	//myStream << std::int << (uint32_t)myHeader->versionNumber;
-	//result( myStream.str() );
+	// convert the versionNumber to string, which is a integer represenation of a integer
 	result = itoa((uint64_t)myHeader->versionNumber, 10);
 	
-
 	
 	// display versionNumber in matrix
 	setCDKMatrixCell(myMatrix, 1, 2, result.c_str());
 	
-	// convert the numRecords to string, which is a integer represenation of a hex number
-	//std::stringstream myStream;
-	//myStream << std::int << (uint64_t)myHeader->numRecords;
-	//result( myStream.str() );
+	// convert the numRecord to string, which is a integer represenation of a integer
 	result = itoa(myHeader->numRecords, 10);
 	
 	// display numRecords in matrix
 	setCDKMatrixCell(myMatrix, 1, 3, result.c_str());
 	
-	const long RECORD_SIZE = sizeof(BinaryFileRecord);
+	
+	
+	// actually draw the matrix from values above
+	drawCDKMatrix(myMatrix, true);
+
+	// keep results on screen
+	sleep (5);	
+	
+	
+	
+	
+	
+	
 	// loop based on number of records found in the header
 	// add to matrix during loop
+	const long RECORD_SIZE = sizeof(BinaryFileRecord);
 	int j = 2;
 	for (int i = 1; i <= (uint32_t)myHeader->numRecords ; i++){
-		// reset j each iteration
+		
+		// reset j each iteration, j controls the column of data insertion into the matrix for this loop
 		j = 1;
 		
 		// read a record from the binary file
@@ -149,37 +150,30 @@ int main()
 		std::string result = itoa((uint64_t)myRecord->strLength, 10);
 		result = "strlen " + result;
 		// display string length in matrix
-		setCDKMatrixCell(myMatrix, i + 1, j, result.c_str());	
+		//setCDKMatrixCell(myMatrix, i + 1, j, result.c_str());	
 		
 		j++;
-		// convert the bugger to string
-		char str[myRecord->strLength];
+		// convert the strBuffer to character array
+		char str[maxRecordStringLength];
 		strcpy (myRecord->stringBuffer,str);
 		// display string length in matrix
-		setCDKMatrixCell(myMatrix, i + 1, j, str);
+		//setCDKMatrixCell(myMatrix, i + 1, j, myRecord->stringBuffer);
 		
-		delete myRecord;
+		//delete myRecord;
+		
+		// move the position for the file with seekg
 		binInfile.seekg (HEADER_SIZE + (i * RECORD_SIZE));
 		
-		//result = itoa((uint64_t)myRecord->stringBuffer, 2);
 		
-		// convert the result to string, which is a integer represenation of a hex number
-		//std::stringstream myStream;
-		//myStream << std::int << (uint64_t)result;
-		//std::string result2( myStream.str() );
-	
-		// display string length in matrix
-		//setCDKMatrixCell(myMatrix, i + 2, j, result.c_str());		
-			
+		cout << "Last record is " << str;
+		
+		//printf("Last record is", std::string(myRecord->stringBuffer));	
 		
 	}	
 	
 	
-	// actually draw the matrix from values above
-	drawCDKMatrix(myMatrix, true);
+	
 
-	// keep results on screen
-	sleep (5);
 
 
 	
